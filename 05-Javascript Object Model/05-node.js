01ddb4ec2b4dd7a6ada466ea0dbc3ee059a512fa
Anntasena <matlailsyahrin@gmail.com>
2023-12-11 22:21:14 +0700

    update belajar

---
+++ 05-Javascript Object Model/05-node.js
@@ -0,0 +1,33 @@
+// Node
+/*
+  > Node adalah base class dari document, element dan attr
+  > Artinya semua fitur yang dimiliki node dimiliki turunannya
+  > Node dalam DOM itu bentuknya adalah tree (pohon), artinya dengan node, kita bisa melihat parent (node diatasnya), children (node dibawahnya), sibling (node disebelahnya)
+*/
+
+// Kode: Node
+/*
+  *Ada di 02-node.html
+*/
+
+// Node Property
+/*
+  > Node memiliki banyak sekali property
+  > Property di node banyak berisi informasi refrence ke node lainnya, seperti ke node parentnya, node childrennya atau node sebelahnya
+*/
+
+// Kode: Node property
+/*
+  *Ada  di 02-node.html
+*/
+
+// Node Method
+/*
+  > Node memiliki banyak sekali method
+  > Method di node bisa digunakan untuk memanipulasi data node di dalam node tersebut, misal menambahkan/menghapus node children
+*/
+
+// Kode: Node method
+/*
+  *Ada  di 02-node.html
+*/
