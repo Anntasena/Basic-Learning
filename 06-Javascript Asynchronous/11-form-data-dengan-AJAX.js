1e3094dd11e63fef370b5eb86e04121f6db1d70b
Anntasena <matlailsyahrin@gmail.com>
2024-01-04 22:24:33 +0700

    update belajar

---
+++ 06-Javascript Asynchronous/11-form-data-dengan-AJAX.js
@@ -0,0 +1,11 @@
+// Form Data dengan AJAX
+/*
+  > Selain JSON dan Query Param, kadang kita juga mengirim Form data dari client ke server menggunakan AJAX
+  > Untuk kasus ini kita tetap bisa menggunakan URLSearchParam
+  > Namun object URLSearchParam nya kita kirim di method send()
+*/
+
+// Kode: Form Data dengan AJAX
+/*
+  *Ada di 08-UJA-from-data-AJAX.html
+*/
---
+++ 06-Javascript Asynchronous/12-upload-file-dengan-ajax.js
@@ -0,0 +1,10 @@
+// Upload File dengan AJAX
+/*
+  > Selain menggunakan JSON, Query Param, Form, kadang kita butuh mengirim file ke server
+  > Untuk mengirim file ke server, kita perlu menggunakan object form data, lalu kita perlu menambahkan object file kedalam formdata teresebut
+*/
+
+// Kode: Upload File dengan AJAX
+/*
+  *Ada di 09-UJA-upload-file-AJAX.html
+*/
