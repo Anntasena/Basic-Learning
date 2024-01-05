2dc8826a307e4fb8b0e13d38d937db26b6efa657
Anntasena <matlailsyahrin@gmail.com>
2024-01-05 22:41:11 +0700

    update belajar

---
+++ 06-Javascript Asynchronous/13-promise.js
@@ -0,0 +1,20 @@
+// Promise
+
+// Apa itu Promise
+/*
+  > Promise merupakan proxy untuk sebuah nilai dimasa depan (Future) yang belum diketahui saat pembuatan promise tersebut.
+  > Biasanya promise digunakan sebagai proxy untuk proses Async
+  > Penggunaan promise sangat mudah, dan lebih mirip dengan kode synchronous
+*/
+
+// Stage dalam promise (Promise State)
+/*
+  > 1. Pending ==> undefined (result)
+  > 2. Fullfiled ==> value (result)
+  > 3. Rejected ==> error (result)
+*/
+
+// Kode: membuat promise
+/*
+  *Ada di 10-UJA-promise.html
+*/
---
+++ 06-Javascript Asynchronous/14-promise-method.js
@@ -0,0 +1,12 @@
+// Promise Method
+/*
+  > Promisem memiliki 3 method yang bisa kita gunakan
+    - then() untuk mendapatkan atau mengubah data ketika promise sukses
+    - catch() untuk mendapatkan data error ketika promise gagal
+    - finally() akan diesekusi ketika terjadi sukses atau gagal pada promise
+*/
+
+// Kode: Promise Method
+/*
+  *Ada di 11-UJA-promise-method.html
+*/
